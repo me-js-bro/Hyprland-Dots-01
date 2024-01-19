c86fb08afa4df360fed20bb12fe11d9b4ff793cf
me-js-bro <ms.js.bro@gmail.com>
2024-01-19 16:12:22 +0600

    updated some changes

--- scripts/toggle_dark_light.sh
+++ scripts/toggle_dark_light.sh
@@ -3,14 +3,14 @@
 # Check which waybar theme is set
 THEMEIS=$(readlink -f ~/.config/waybar/style.css | cut -d '-' -f2)
 rofi=$(readlink -f ~/.config/rofi/config.rasi | cut -d '-' -f2)
+alacritty=$(readlink -f ~/.config/alacritty/alacritty.toml | cut -d '-' -f2)
+
+# vs code settings
+vs_code_settings=~/.config/Code/User/settings.json
 
 #if the theme is not dark then we need to switch to it
 if [ $THEMEIS != "light.css" ]; then
     SWITCHTO="-light"
-fi
-
-if [ $rofi != "light.rasi" ]; then
-    switch_to="-light"
 fi
 
 #change the background image and be cool about it ;)
@@ -21,16 +21,25 @@ swww img ~/.config/hypr/Wallpaper/dynamic/wallpaper$SWITCHTO.jpg --transition-fp
 
 #change gtk theme
 gsettings set org.gnome.desktop.interface gtk-theme "theme$SWITCHTO"
-# gsettings set icon-theme ""
 
 #set the wofi theme
 ln -sf ~/.config/wofi/style/style$SWITCHTO.css ~/.config/wofi/style.css
 
 #set rofi theme
-ln -sf ~/.config/rofi/themes/config$switch_to.rasi ~/.config/rofi/config.rasi
+ln -sf ~/.config/rofi/themes/config$SWITCHTO.rasi ~/.config/rofi/config.rasi
 
 #set the waybar theme
 ln -sf ~/.config/waybar/style/style$SWITCHTO.css ~/.config/waybar/style.css
+
+# change alacritty theme
+ln -sf ~/.config/alacritty/themes/alacritty$SWITCHTO.toml ~/.config/alacritty/alacritty.toml
+
+# change vs code theme
+if [ $THEMEIS != "light.css" ]; then
+    sed -i 's/"workbench.colorTheme": "Theme Flat"/"workbench.colorTheme": "Atom One Light"/g' "$vs_code_settings"
+else
+    sed -i 's/"workbench.colorTheme": "Atom One Light"/"workbench.colorTheme": "Theme Flat"/g' "$vs_code_settings"
+fi
 
 #restart the waybar
 killall -SIGUSR2 waybar
