e88d5415fb0c71d6f62ca55b99ba43626507d659
me-js-bro <me.js.bro@gmail.com>
2024-02-23 09:05:30 +0600

    Added numbers of clipboard list

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -4,7 +4,6 @@ if [ -z "$XDG_PICTURES_DIR" ] ; then
     XDG_PICTURES_DIR="$HOME/Pictures"
 fi
 
-ScrDir=`dirname $(realpath $0)`
 swpy_dir="$HOME/.config/swappy"
 save_dir="${2:-$XDG_PICTURES_DIR/Screenshots}"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
